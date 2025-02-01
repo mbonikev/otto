95eacb175c080c67b4800ea428c9683e380772ba
mbonikev <mbonimpayekevin@gmail.com>
2025-02-01 15:30:51 +0200

    updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ export default {
       fontFamily:{
         Inter: ["Inter", "sans-serif"],
         LilitaOne: ["Lilita One", "sans-serif"],
+        Kanit: ["Kanit", "sans-serif"],
       },
     },
   },
