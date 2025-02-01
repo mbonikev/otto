10c5c343f5c1d410a50d6c35dfb23c6b11c2af7d
mbonikev <mbonimpayekevin@gmail.com>
2025-02-01 15:08:30 +0200

    updated src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,19 @@ import React from "react";
 import "./index.css";
 import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
 
 function App() {
   const hasHash = window.location.hash.includes("#");
   if (!hasHash && window.location.pathname !== "/") {
-    return <Home />;
+    return <NotFound />;
   }
   return (
     <>
       <HashRouter>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="*" element={<Home />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </HashRouter>
     </>
