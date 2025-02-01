1be150ba7ebb1e0181858879845095d07a61b661
mbonikev <mbonimpayekevin@gmail.com>
2025-02-01 18:41:42 +0200

    updated src/components/PromptArea/PromptArea.jsx

--- src/components/PromptArea/PromptArea.jsx
+++ src/components/PromptArea/PromptArea.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function PromptArea() {
   return (
-    <div className='w-full h-[300px] p-2'>
-      <div className='w-full max-w-[800px] rounded-2xl ring-1 ring-stone-200'></div>
+    <div className='w-full h-[130px] p-2'>
+      <div className='w-full max-w-[800px] h-full rounded-2xl ring-1 ring-stone-200'></div>
     </div>
   )
 }
