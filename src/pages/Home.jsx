3e67b91338733e45becccf6c28fe8c09afecc3d7
mbonikev <mbonimpayekevin@gmail.com>
2025-02-01 19:18:56 +0200

    updated src/pages/Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,20 @@
-import React from 'react'
-import Navbar from '../components/Navbar/Navbar'
-import PromptArea from '../components/PromptArea/PromptArea'
+import React from "react";
+import Navbar from "../components/Navbar/Navbar";
+import PromptArea from "../components/PromptArea/PromptArea";
 
 function Home() {
   return (
-    <div className='w-full h-svh flex flex-col'>
+    <div className="w-full h-svh flex flex-col">
       <Navbar />
-      <div className='w-full flex-1 flex flex-col'>
+      <div className="w-full flex-1 flex flex-col">
         {/* empty */}
-        <div className='w-full h-full flex flex-col items-center justify-center'>
-          <h1 className='text-3xl font-semibold'>What can I help with?</h1>
+        <div className="w-full h-full flex flex-col items-center justify-center">
+          <h1 className="text-3xl font-semibold">How can I assist you?</h1>
         </div>
       </div>
       <PromptArea />
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
