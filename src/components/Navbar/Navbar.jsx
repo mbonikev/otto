7742866cdf0ef3f57973392ad6acb6960d0e2458
mbonikev <mbonimpayekevin@gmail.com>
2025-02-01 16:55:09 +0200

    updated src/components/Navbar/Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -27,7 +27,7 @@ function Navbar() {
       {/* 3 */}
       <div className="flex items-center justify-end gap-1">
         <button className="rounded-full overflow-hidden">
-          <img src="/avatars/avatar1.png" alt="avatar" className="bg-stone-100 h-9 w-9 rounded-full" />
+          <img src="/avatars/avatar3.png" alt="avatar" className="bg-stone-100 h-9 w-9 rounded-full" />
         </button>
       </div>
     </div>
