523440fad904921754cf90f9394e20450ed1b325
mbonikev <mbonimpayekevin@gmail.com>
2025-02-01 17:14:21 +0200

    updated src/components/Navbar/Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -85,7 +85,7 @@ function Navbar() {
           >
             <button className="w-full rounded-xl p-2 flex items-center justify-start gap-2 hover:bg-stone-100">
               <HiOutlineUser className="text-2xl text-dark-text-weak/70" />
-              <h1 className="text-sm">My account</h1>
+              <h1 className="text-sm">Account</h1>
             </button>
             <button className="w-full rounded-xl p-2 flex items-center justify-start gap-2 hover:bg-stone-100">
               <HiOutlineCog6Tooth className="text-2xl text-dark-text-weak/70" />
