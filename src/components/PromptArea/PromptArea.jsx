6ed52386311dce1f16913c1c7fa5a22a5aeaebb4
mbonikev <mbonimpayekevin@gmail.com>
2025-02-01 18:46:52 +0200

    updated src/components/PromptArea/PromptArea.jsx

--- src/components/PromptArea/PromptArea.jsx
+++ src/components/PromptArea/PromptArea.jsx
@@ -1,9 +1,12 @@
+import TextArea from 'antd/es/input/TextArea'
 import React from 'react'
 
 function PromptArea() {
   return (
     <div className='w-full h-fit flex flex-col gap-3 p-3'>
-      <div className='w-full max-w-[800px] mx-auto h-[120px] rounded-[25px] shadow-lg ring-1 ring-stone-200'></div>
+      <div className='w-full max-w-[800px] mx-auto h-[120px] rounded-[25px] shadow-lg ring-1 ring-stone-200'>
+        <TextArea />
+      </div>
       <h1 className='text-xs font-normal text-dark-text-weak w-fit mx-auto'>Otto can make mistakes. still in beta.</h1>
     </div>
   )
