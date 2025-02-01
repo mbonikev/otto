745932f44879a8897146c896a6db85833373fdc8
mbonikev <mbonimpayekevin@gmail.com>
2025-02-01 19:01:24 +0200

    updated src/components/PromptArea/PromptArea.jsx

--- src/components/PromptArea/PromptArea.jsx
+++ src/components/PromptArea/PromptArea.jsx
@@ -23,7 +23,7 @@ function PromptArea() {
           onChange={handleInput2Change}
           rows="1"
           placeholder="Message Otto"
-          className="text-base font-norma bg-transparent min-h-[80px] max-h-[150px] px-4 pt-4 pb-0 outline-none w-full placeholder:text-dark-text-weak text-dark-text resize-none overflow-hidden "
+          className="text-base font-norma bg-transparent min-h-[80px] max-h-[150px] px-3 pt-3 pb-0 outline-none w-full placeholder:text-dark-text-weak text-dark-text resize-none overflow-hidden "
         ></textarea>
         <div className="flex items-center justify-between w-full  min-h-[40px] bg-red-400/0 p-2">
           {/* 1 */}
