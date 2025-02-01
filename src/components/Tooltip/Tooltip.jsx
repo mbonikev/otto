d417824addb0415657d693fe8aada9be8ac7d38c
mbonikev <mbonimpayekevin@gmail.com>
2025-02-01 15:59:04 +0200

    updated src/components/Tooltip/Tooltip.jsx

--- src/components/Tooltip/Tooltip.jsx
+++ src/components/Tooltip/Tooltip.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-function Tooltip({ children }) {
+function Tooltip({ children, title }) {
   return (
     <div className="h-fit w-fit relative">
       {children}
       <div className="asbolute top-[110%] bg-dark-text text-white ">
-        Chats
+        
       </div>
     </div>
   );
