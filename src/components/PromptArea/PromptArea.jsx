226b3d7482b7d0f98f18241f45e52f331cb9680d
mbonikev <mbonimpayekevin@gmail.com>
2025-02-01 18:44:42 +0200

    updated src/components/PromptArea/PromptArea.jsx

--- src/components/PromptArea/PromptArea.jsx
+++ src/components/PromptArea/PromptArea.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function PromptArea() {
   return (
     <div className='w-full h-fit p-4 flex flex-col gap-3'>
-      <div className='w-full max-w-[800px] mx-auto h-[120px] rounded-[30px] shadow-lg ring-1 ring-stone-300'></div>
+      <div className='w-full max-w-[800px] mx-auto h-[120px] rounded-[30px] shadow-lg ring-1 ring-stone-200'></div>
       <h1 className='text-xs font-normal text-dark-text-weak w-fit mx-auto'>ChatGPT can make mistakes. Check important info.</h1>
     </div>
   )
