39f4830cae276959fa48a5a0c0f33417923b3300
mbonikev <mbonimpayekevin@gmail.com>
2025-02-01 15:59:34 +0200

    updated src/components/Tooltip/Tooltip.jsx

--- src/components/Tooltip/Tooltip.jsx
+++ src/components/Tooltip/Tooltip.jsx
@@ -4,7 +4,7 @@ function Tooltip({ children, title }) {
   return (
     <div className="h-fit w-fit relative">
       {children}
-      <div className="asbolute top-[110%] bg-dark-text text-white text-xs font-semibold">
+      <div className="asbolute top-[110%] bg-dark-text text-white/80 text-xs font-semibold">
         {title}
       </div>
     </div>
