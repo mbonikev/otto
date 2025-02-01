17696aea92d432c4a4af67d6e96816c8bcefe674
mbonikev <mbonimpayekevin@gmail.com>
2025-02-01 15:28:01 +0200

    updated src/components/Navbar/Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div className='w-full h-[50px] bg-emerald-300 grid grid-cols-3'>
+    <div className='w-full h-[70px] grid grid-cols-3'>
         {/* 1 */}
         <div className='flex items-center justify-start gap-1'></div>
         {/* 2 */}
