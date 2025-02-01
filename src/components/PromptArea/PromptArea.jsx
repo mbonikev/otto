15381ffd435334e10c3a8ec992b0b4780fa7baa3
mbonikev <mbonimpayekevin@gmail.com>
2025-02-01 18:43:12 +0200

    updated src/components/PromptArea/PromptArea.jsx

--- src/components/PromptArea/PromptArea.jsx
+++ src/components/PromptArea/PromptArea.jsx
@@ -4,6 +4,7 @@ function PromptArea() {
   return (
     <div className='w-full h-[120px] p-4'>
       <div className='w-full max-w-[800px] h-full rounded-3xl shadow-lg ring-1 ring-stone-300'></div>
+      <h1 className='text-xs font-normal text-dark-text-weak'>ChatGPT can make mistakes. Check important info.</h1>
     </div>
   )
 }
