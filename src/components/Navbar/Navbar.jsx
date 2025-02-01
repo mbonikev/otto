1e86045d0ce86035ac2097ab559971b3632a393e
mbonikev <mbonimpayekevin@gmail.com>
2025-02-01 16:01:34 +0200

    updated src/components/Navbar/Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -9,7 +9,7 @@ function Navbar() {
       <div className="flex items-center justify-start gap-1">
         <button className="p-2.5 text-xl bg-stone-100 hover:bg-stone-200 active:scale-95 rounded-full relative">
           <HiOutlineChatBubbleLeftRight />
-          <div className="asbolute top-[110%] left-0 bg-dark-text text-white/90 text-xs font-semibold px-2">New chat</div>
+          <div className="asbolute top-[110%] left-0 bg-dark-text text-white/90 text-xs font-semibold px-2">Chat history</div>
         </button>
         <button className="p-2.5 text-xl bg-stone-100 hover:bg-stone-200 active:scale-95 rounded-full relative">
           <HiMiniPlus />
