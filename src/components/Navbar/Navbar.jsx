8d27ce367b9f39f906088a0fabde3e17407d1e14
mbonikev <mbonimpayekevin@gmail.com>
2025-02-01 17:12:48 +0200

    updated src/components/Navbar/Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -83,14 +83,14 @@ function Navbar() {
             className="absolute top-[90%] w-[220px] h-fit rounded-2xl ring-1 ring-stone-200 shadow-lg p-1.5"
           >
             <button className="w-full rounded-xl p-2 flex items-center justify-start gap-2 hover:bg-stone-100">
-              <HiOutlineCog6Tooth className="text-xl text-dark-text-weak/50" />
+              <HiOutlineCog6Tooth className="text-2xl text-dark-text-weak/70" />
               <h1 className="text-sm">Settings</h1>
             </button>
             {/*  */}
             <div className="w-[90%] h-[1px] bg-stone-200 mx-auto my-1"></div>
             {/*  */}
             <button className="w-full rounded-xl p-2 flex items-center justify-start gap-2 hover:bg-stone-100">
-              <HiOutlineArrowRightStartOnRectangle className="text-xl text-dark-text-weak/50" />
+              <HiOutlineArrowRightStartOnRectangle className="text-2xl text-dark-text-weak/70" />
               <h1 className="text-sm">Sign Out</h1>
             </button>
           </div>
