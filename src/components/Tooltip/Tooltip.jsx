dbf75ddf9b859fec253bf1bdf8bb28cacc600bc8
mbonikev <mbonimpayekevin@gmail.com>
2025-02-01 16:09:45 +0200

    updated src/components/Tooltip/Tooltip.jsx

--- src/components/Tooltip/Tooltip.jsx
+++ src/components/Tooltip/Tooltip.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Tooltip({ title }) {
   return (
-    <div className="absolute top-[115%] w-fit whitespace-nowrap px-2.5 py-2 rounded-[9px] left-0 bg-dark-text text-white/80 text-xs font-semibold">
+    <div className="absolute top-[130%] w-fit whitespace-nowrap px-2.5 py-2 rounded-[9px] left-0 bg-[#333] text-white/80 text-xs font-semibold">
       {title}
     </div>
   );
