7df71e754052f46d602584b106b3d091c3d47f7e
mbonikev <mbonimpayekevin@gmail.com>
2025-02-01 15:31:42 +0200

    updated src/components/Navbar/Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -10,7 +10,7 @@ function Navbar() {
         <div className="w-7 min-w-7">
           <img src="/logo.png" alt="logo" className="w-full h-fit " />
         </div>
-        <h1 className="font-Kanit text-2xl font-normal">Otto</h1>
+        <h1 className="font-Kanit text-2xl font-light">Otto</h1>
       </div>
       {/* 3 */}
       <div className="flex items-center justify-end gap-1"></div>
