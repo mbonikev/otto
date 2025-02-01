6524ce835c865b61df43f50ebabe1d8df7b9d728
mbonikev <mbonimpayekevin@gmail.com>
2025-02-01 18:47:02 +0200

    updated src/components/PromptArea/PromptArea.jsx

--- src/components/PromptArea/PromptArea.jsx
+++ src/components/PromptArea/PromptArea.jsx
@@ -5,7 +5,7 @@ function PromptArea() {
   return (
     <div className='w-full h-fit flex flex-col gap-3 p-3'>
       <div className='w-full max-w-[800px] mx-auto h-[120px] rounded-[25px] shadow-lg ring-1 ring-stone-200'>
-        <TextArea />
+        
       </div>
       <h1 className='text-xs font-normal text-dark-text-weak w-fit mx-auto'>Otto can make mistakes. still in beta.</h1>
     </div>
