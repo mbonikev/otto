3209178f615c13be44b21ae8bd129b71853b1850
mbonikev <mbonimpayekevin@gmail.com>
2025-02-01 19:08:55 +0200

    updated src/components/PromptArea/PromptArea.jsx

--- src/components/PromptArea/PromptArea.jsx
+++ src/components/PromptArea/PromptArea.jsx
@@ -29,7 +29,7 @@ function PromptArea() {
         <div className="flex items-center justify-between w-full h-[32px] bg-red-400/0 min-h-[32px] px-1.5 mb-1.5">
           {/* 1 */}
           <div className="w-full h-full">
-            <button className="group h-full w-fit px-3 flex items-center justify-center text-sm ring-1 ring-stone-300 text-dark-text-weak font-semibold rounded-full relative">
+            <button className="group h-full w-fit px-3 flex items-center justify-center gap-1 text-sm ring-1 ring-stone-300 text-dark-text-weak font-semibold rounded-full relative">
             <RiRobot3Line className="text-base" />
             Llama 3.1
             </button>
