b5d94dfbd7164f854ab5740cc62ed1220ac99571
mbonikev <mbonimpayekevin@gmail.com>
2025-02-01 18:40:31 +0200

    updated src/pages/Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from '../components/Navbar/Navbar'
+import PromptArea from '../components/PromptArea/PromptArea'
 
 function Home() {
   return (
     <div className='w-full h-svh'>
       <Navbar />
-      
+      <div className=''></div>
+      <PromptArea />
     </div>
   )
 }
