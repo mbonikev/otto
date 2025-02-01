36f42d1ee56c10da371559379054b3ff7d2eba62
mbonikev <mbonimpayekevin@gmail.com>
2025-02-01 15:23:51 +0200

    updated src/components/Navbar/Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,7 +4,7 @@ function Navbar() {
   return (
     <div className='w-full h-[50px] bg-emerald-300 grid grid-cols-3'>
         {/* 1 */}
-        <div className=''></div>
+        <div className='flex items-center justify-start gap-1'></div>
     </div>
   )
 }
