0cfdd1a454c054a224915aee2ea7004a44a5ffad
mbonikev <mbonimpayekevin@gmail.com>
2025-02-01 15:05:40 +0200

    updated src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import { HashRouter, Routes, Route } from 'react-router-dom'
+import Home from "./pages/Home"
 
 function App() {
   return (
-    <div>App</div>
+    <>
+      <HashRouter>
+        <Routes>
+          <Route path='/' element={<Home />}/>
+        </Routes>
+      </HashRouter>
+    </div>
   )
 }
 
