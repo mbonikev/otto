6f4a2af70935439683dcb4f0e06fae9492f8a262
mbonikev <mbonimpayekevin@gmail.com>
2025-02-01 16:19:06 +0200

    updated src/components/Tooltip/Tooltip.jsx

--- src/components/Tooltip/Tooltip.jsx
+++ src/components/Tooltip/Tooltip.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Tooltip({ title, placement }) {
   return (
-    <div className={`absolute top-[125%] w-fit whitespace-nowrap px-3 py-1 rounded-[9px] left-0 bg-[#1b1b1b] text-white/90 text-[13px] font-semibold ${placement === "left" ? "left-0" : placement === "center" ? "" : ""}`}>
+    <div className={`absolute top-[125%] w-fit whitespace-nowrap px-3 py-1 rounded-[9px] left-0 bg-[#1b1b1b] text-white/90 text-[13px] font-semibold ${placement === "left" ? "left-0" : placement === "center" ? "left-0 right-0 mx-0" : ""}`}>
         <div className={`w-2 h-2 bg-[#1b1b1b] absolute -top-[4px] rotate-45 ${placement  === "left" ? "left-[16px]" : placement === "center" ? "left-[-50%] right-[-50%] mx-auto" : "right-[16px]"}`}></div>
       {title}
     </div>
