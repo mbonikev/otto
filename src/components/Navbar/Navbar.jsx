665660dc3c8557e858adb79909c7535950d56dd8
mbonikev <mbonimpayekevin@gmail.com>
2025-02-01 15:23:41 +0200

    updated src/components/Navbar/Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div className='w-full h-[50px] bg-emerald-300 grid grid-cols-3'>Navbar</div>
+    <div className='w-full h-[50px] bg-emerald-300 grid grid-cols-3'>
+        {/* 1 */}
+        <div className=''></div>
+    </div>
   )
 }
 
