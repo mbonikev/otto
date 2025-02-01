8776213c5ca4e108dd29d2b59e625648be11a103
mbonikev <mbonimpayekevin@gmail.com>
2025-02-01 15:58:24 +0200

    updated src/components/Tooltip/Tooltip.jsx

--- src/components/Tooltip/Tooltip.jsx
+++ src/components/Tooltip/Tooltip.jsx
@@ -4,7 +4,9 @@ function Tooltip({ children }) {
   return (
     <div className="h-fit w-fit relative">
       {children}
-      <div className="asbolute top-[110%]"></div>
+      <div className="asbolute top-[110%] bg-dark-text ">
+        
+      </div>
     </div>
   );
 }
