940e2ee2600d5fb43598a48edaf765f78bd7a95d
mbonikev <mbonimpayekevin@gmail.com>
2025-02-01 17:55:27 +0200

    updated src/components/Navbar/Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -68,7 +68,7 @@ function Navbar() {
           className={`w-fit h-svh fixed top-0 left-0 transition-transform duration-300 ease-in-out z-30
                 ${
                   animateChatsModal
-                    ? "translate-x-0 opacity-100"
+                    ? "translate-x-0"
                     : "-translate-x-[300px]"
                 }`}
         >
