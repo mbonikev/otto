e3473fa885c73d9865825006ea8c405a76e520d0
mbonikev <mbonimpayekevin@gmail.com>
2025-02-01 18:59:54 +0200

    updated src/components/PromptArea/PromptArea.jsx

--- src/components/PromptArea/PromptArea.jsx
+++ src/components/PromptArea/PromptArea.jsx
@@ -30,7 +30,7 @@ function PromptArea() {
           <div></div>
           {/* 2 */}
           <div className="w-fit h-full">
-            <button className="group h-10 w-auto aspect-square flex items-center justify-center text-3  xl text-dark-text rounded-full relative">
+            <button className="group h-ull w-auto aspect-square flex items-center justify-center text-3xl text-dark-text rounded-full relative">
               <BsArrowUpCircleFill />
             </button>
           </div>
