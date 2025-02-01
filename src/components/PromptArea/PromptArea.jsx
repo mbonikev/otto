7af39d02c9e21cb7e3d250fdf9e89cc87ff06e06
mbonikev <mbonimpayekevin@gmail.com>
2025-02-01 19:08:25 +0200

    updated src/components/PromptArea/PromptArea.jsx

--- src/components/PromptArea/PromptArea.jsx
+++ src/components/PromptArea/PromptArea.jsx
@@ -1,6 +1,7 @@
 import TextArea from "antd/es/input/TextArea";
 import React, { useRef, useState } from "react";
 import { BsArrowUpCircleFill } from "react-icons/bs";
+import { RiRobot3Line } from "react-icons/ri";
 
 function PromptArea() {
   const [message, setMessage] = useState("");
@@ -29,6 +30,7 @@ function PromptArea() {
           {/* 1 */}
           <div className="w-full h-full">
             <button className="group h-full w-fit px-3 flex items-center justify-center text-sm ring-1 ring-stone-300 text-dark-text-weak font-semibold rounded-full relative">
+            <RiRobot3Line className="text-lg" />
             Llama 3.1
             </button>
           </div>
