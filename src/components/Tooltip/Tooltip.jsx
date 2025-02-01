d03ed7ff56bfae0e110b1fdce52d1a5a28ebc315
mbonikev <mbonimpayekevin@gmail.com>
2025-02-01 15:57:54 +0200

    updated src/components/Tooltip/Tooltip.jsx

--- src/components/Tooltip/Tooltip.jsx
+++ src/components/Tooltip/Tooltip.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Tooltip({children}) {
   return (
-    <div className='h-fit w-fit'>{children}</div>
+    <div className='h-fit w-fit relative'>{children}</div>
   )
 }
 
