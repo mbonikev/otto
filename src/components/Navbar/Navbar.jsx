022b743fa5508d81a00344b7e860887371610b04
mbonikev <mbonimpayekevin@gmail.com>
2025-02-01 17:10:03 +0200

    updated src/components/Navbar/Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -80,7 +80,7 @@ function Navbar() {
             ref={popupRef}
             className="absolute top-[90%] w-[220px] h-fit rounded-2xl ring-1 ring-stone-200 shadow-lg"
           >
-            <button className="w-full rounded-lg p-2 flex items-center justify-start gap-1">
+            <button className="w-full rounded-lg p-2 flex items-center justify-start gap-1 hover:bg-stone-100">
               <HiOutlineArrowLeftStartOnRectangle className="text-xl" />
               <h1 className="text-sm">Sign Out</h1>
             </button>
