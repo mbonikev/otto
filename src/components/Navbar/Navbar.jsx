5ca32deb982d1f03cfba5d085fb8fe6d32a3d3ad
mbonikev <mbonimpayekevin@gmail.com>
2025-02-01 15:28:31 +0200

    updated src/components/Navbar/Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,21 +1,21 @@
-import React from 'react'
+import React from "react";
 
 function Navbar() {
   return (
-    <div className='w-full h-[70px] grid grid-cols-3'>
-        {/* 1 */}
-        <div className='flex items-center justify-start gap-1'></div>
-        {/* 2 */}
-        <div className='flex items-center justify-center gap-1'>
-            <div className='w-7 min-w-7'>
-                <img src="/logo.png" alt="logo" className='w-full h-fit ' />
-                <h1>Otto</h1>
-            </div>
+    <div className="w-full h-[70px] grid grid-cols-3">
+      {/* 1 */}
+      <div className="flex items-center justify-start gap-1"></div>
+      {/* 2 */}
+      <div className="flex items-center justify-center gap-1">
+        <div className="w-7 min-w-7">
+          <img src="/logo.png" alt="logo" className="w-full h-fit " />
         </div>
-        {/* 3 */}
-        <div className='flex items-center justify-end gap-1'></div>
+        <h1>Otto</h1>
+      </div>
+      {/* 3 */}
+      <div className="flex items-center justify-end gap-1"></div>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
