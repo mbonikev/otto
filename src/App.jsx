98d020debee4421159a191ab0c2d009ee254f829
mbonikev <mbonimpayekevin@gmail.com>
2025-02-01 15:07:20 +0200

    updated src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./index.css";
 import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
