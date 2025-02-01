d5b726c223b6a159e756a1727e39849a04dae679
mbonikev <mbonimpayekevin@gmail.com>
2025-02-01 17:12:17 +0200

    updated src/components/Navbar/Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -82,7 +82,7 @@ function Navbar() {
             className="absolute top-[90%] w-[220px] h-fit rounded-2xl ring-1 ring-stone-200 shadow-lg p-1.5"
           >
             <button className="w-full rounded-xl p-2 flex items-center justify-start gap-2 hover:bg-stone-100">
-              <HiOutlineArrowRightStartOnRectangle className="text-xl text-dark-text-weak/50" />
+              <HiOutlineCog6Toott className="text-xl text-dark-text-weak/50" />
               <h1 className="text-sm">Sign Out</h1>
             </button>
             {/*  */}
