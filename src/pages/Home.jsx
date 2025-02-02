2d27e7386f94e60b8e2af2e6c4026c28a2f40bbc
mbonikev <mbonimpayekevin@gmail.com>
2025-02-02 04:39:35 +0200

    updated src/pages/Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -19,7 +19,7 @@ function Home() {
       });
     }
     if (messages.length > 0) {
-      document.title = "Otto - " + messages[0].content.slice(0, 20);
+      document.title = "Otto - " + messages[1].title;
     }
     if (messages.length < 1) {
       document.title = "Otto";
