2b00d236bae0d5a82ca660103f9228eba1e21adb
mbonikev <mbonimpayekevin@gmail.com>
2025-02-02 06:32:01 +0200

    updated src/pages/Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,7 @@ import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { oneLight } from "react-syntax-highlighter/dist/esm/styles/prism";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
+import { useOutletContext } from "react-router-dom";
 
 function Home() {
   const { username, picture } = useOutletContext();
