00fb2c024e682c50622869c3978ed3b4a16cebb6
mbonikev <mbonimpayekevin@gmail.com>
2025-02-02 05:12:16 +0200

    updated src/pages/Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -50,7 +50,7 @@ function Home() {
                 language="javascript"
                 style={oneLight}
                 className={`rounded-2xl overflow-x-auto max-w-[100%] p-6 break-words whitespace-pre-line
-                  ${role === "assistant" ? "" : ""}`}
+                  ${role === "assistant" ? "hide_fist_span" : ""}`}
               >
                 {part}
               </SyntaxHighlighter>
