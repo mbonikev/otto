4de6e06e13635720c1a9e9e51182f68e15d8febe
mbonikev <mbonimpayekevin@gmail.com>
2025-02-02 06:29:31 +0200

    updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,15 +3,18 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      fontFamily:{
+      fontFamily: {
         Inter: ["Inter", "sans-serif"],
         Kanit: ["Kanit", "sans-serif"],
       },
-      colors:{
+      colors: {
         "main-color": "#0066ff",
         "dark-text": "#111111",
         "dark-text-weak": "#5d5d5d",
         "": "",
+      },
+      animation: {
+        spinLoader: "spin .4s linear infinite",
       },
     },
   },
