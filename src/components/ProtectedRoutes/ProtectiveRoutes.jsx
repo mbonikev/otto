08bc28a52a1d7d9c14389d59940690c280f63fdd
mbonikev <mbonimpayekevin@gmail.com>
2025-02-02 06:04:29 +0200

    updated src/components/ProtectedRoutes/ProtectiveRoutes.jsx

--- src/components/ProtectedRoutes/ProtectiveRoutes.jsx
+++ src/components/ProtectedRoutes/ProtectiveRoutes.jsx
@@ -1,25 +1,9 @@
 import { useEffect, useState } from "react";
 import { Outlet, Navigate } from "react-router-dom";
 const ProtectedRoutes = () => {
-  const [userId, setUserId] = useState(null);
-  const [username, setUserName] = useState(null);
-  const [userEmail, setUserEmail] = useState(null);
+  const [user, setUser] = useState(null);
 
-  useEffect(() => {
-    const email = localStorage.getItem("upfront_user");
-    const name = localStorage.getItem("upfront_user_name");
-    const workspaces = JSON.parse(localStorage.getItem("upfront_ws")) || [];
-    if (email) {
-      setUser(true);
-      setUserName(name);
-      setUserEmail(email);
-      setWorkspaces(workspaces);
-    } else {
-      setUser(false);
-      setUserName(null);
-      setUserEmail(null);
-    }
-  }, []);
+
   if (user === null) {
     return <></>;
   }
