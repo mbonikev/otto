6b4d06e3169216c717b7722917bafd0d5e8ba683
mbonikev <mbonimpayekevin@gmail.com>
2025-02-03 16:30:52 +0200

    updated src/components/Navbar/Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -127,7 +127,7 @@ function Navbar({ photo, displayName }) {
       <div className="flex items-center justify-center gap-1 select-none w-full max-w-[100px] mx-auto relative group bg-yellow-100">
         {/* info */}
         <div className="w-[210px] h-0 group-hover:h-[200px] opacity-0 group-hover:opacity-100 transition-all bg-white ring-1 absolute top-2 mx-auto z-10"></div>
-        <div className="w-7 min-w-7 z-20 group-hover:w-16 translate-y-5">
+        <div className="w-7 min-w-7 z-20 group-hover:w-16 translate-y-5 transition-all">
           <img src="/logo.png" alt="logo" className="w-full h-fit " />
         </div>
         {/* <h1 className="font-Kanit text-2xl font-light text-dark-text-weak">
