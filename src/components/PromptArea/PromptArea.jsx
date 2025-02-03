c5dc61a47497675fe0cfc64ad45ee411daa95b4b
mbonikev <mbonimpayekevin@gmail.com>
2025-02-03 18:41:53 +0200

    updated src/components/PromptArea/PromptArea.jsx

--- src/components/PromptArea/PromptArea.jsx
+++ src/components/PromptArea/PromptArea.jsx
@@ -117,7 +117,7 @@ function PromptArea({
               <Tooltip title="Type Codes" placement="right-center" />
             </div>
             <div className="group cursor-pointer h-full w-fit px-3 flex items-center justify-center gap-1 text-sm ring-1 ring-stone-300 text-dark-text-weak hover:text-dark-text font-semibold rounded-full relative">
-              <BsFileEarmarkText className="text-lg text-red-600" />
+              <BsFileEarmarkText className="text-base text-red-600" />
               Summarize
               <Tooltip title="Models" placement="right-center" />
             </div>
