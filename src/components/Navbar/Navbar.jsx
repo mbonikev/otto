d010d9d0d3f09766aa7765016591645948858bf4
mbonikev <mbonimpayekevin@gmail.com>
2025-02-03 18:33:32 +0200

    updated src/components/Navbar/Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -166,6 +166,10 @@ function Navbar({ photo, displayName }) {
           <IoShareSocialOutline />
           <Tooltip title="Share" placement="center" />
         </button>
+        <button className="group h-10 w-auto aspect-square flex items-center justify-center text-2xl hover:bg-stone-100 text-dark-text-weak hover:text-dark-text rounded-full relative">
+          <BsIncognito />
+          <Tooltip title="Temporary Mode" placement="center" />
+        </button>
         <button
           ref={profileRef}
           onClick={() => setShowPopup(true)}
