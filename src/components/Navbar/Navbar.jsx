e505009cef91ca264b098cb596bc0472acf564db
mbonikev <mbonimpayekevin@gmail.com>
2025-02-03 17:07:26 +0200

    updated src/components/Navbar/Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -142,7 +142,7 @@ function Navbar({ photo, displayName }) {
           <div className="w-full flex flex-col">
             <div className="w-full h-9 bg-red-200 flex items-center justify-between">
               <div className="flex-1 flex">
-                <div className="w-19 h-19 aspect-square flex justify-center items-center rounded-full bg-stone-200/50 text-dark-text text-lg"><BsStars /></div>
+                <div className="w-9 h-19 aspect-square flex justify-center items-center rounded-full bg-stone-200/50 text-dark-text text-lg"><BsStars /></div>
                 <span></span>
               </div>
             </div>
