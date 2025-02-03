da7b5c113f029dda4d41381f4b1bb47fb69300e2
mbonikev <mbonimpayekevin@gmail.com>
2025-02-03 15:23:18 +0200

    updated src/components/Navbar/Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -157,7 +157,7 @@ function Navbar({ photo, username }) {
             />
           )}
         </button>
-        {username}
+        {username}ss
         {/* dropdown */}
         {showPopup && (
           <div
