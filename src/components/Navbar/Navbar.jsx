903e4449d7133ee983707b534d9b96c4a2f15644
mbonikev <mbonimpayekevin@gmail.com>
2025-02-03 16:37:23 +0200

    updated src/components/Navbar/Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -129,8 +129,8 @@ function Navbar({ photo, displayName }) {
         <div className="w-0 h-0 group-hover:w-[210px] delay-150 group-hover:h-[180px] opacity-0 group-hover:opacity-100 transition-all bg-transparent absolute top-0 mx-auto z-10 pt-2">
           <div className="w-full h-full bg-white ring-1 ring-stone-200 shadow-lg rounded-3xl pt-[90px]">
             <h1 className="font-Kanit text-2xl font-light text-dark-text-weak w-full text-center">
-          Otto
-        </h1>
+              Otto
+            </h1>
           </div>
         </div>
         <div className="w-7 min-w-7 z-20 group-hover:w-16 delay-150 translate-y-5 transition-all">
