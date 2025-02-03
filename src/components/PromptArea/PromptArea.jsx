c6048304196e7387a8f7396eb42eb33b7e69e603
mbonikev <mbonimpayekevin@gmail.com>
2025-02-03 17:26:18 +0200

    updated src/components/PromptArea/PromptArea.jsx

--- src/components/PromptArea/PromptArea.jsx
+++ src/components/PromptArea/PromptArea.jsx
@@ -104,8 +104,8 @@ function PromptArea({ setMessages, thinking, setThinking, displayName, email }) 
             </div>
             <div className="group cursor-pointer h-full w-fit px-3 flex items-center justify-center gap-1 text-sm ring-1 ring-stone-300 text-dark-text-weak hover:text-dark-text font-semibold rounded-full relative">
               <GoCommandPalette className="text-lg" />
-              llama3-8b-8192
-              <Tooltip title="Models" placement="right-center" />
+              Code
+              <Tooltip title="Send Codes" placement="right-center" />
             </div>
             <div className="group cursor-pointer h-full w-fit px-3 flex items-center justify-center gap-1 text-sm ring-1 ring-stone-300 text-dark-text-weak hover:text-dark-text font-semibold rounded-full relative">
               <LuBrain className="text-lg" />
