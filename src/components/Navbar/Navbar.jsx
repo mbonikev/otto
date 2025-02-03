0cdde1e96cf8a09ac04f03c438b95f8d83acde28
mbonikev <mbonimpayekevin@gmail.com>
2025-02-03 16:27:52 +0200

    updated src/components/Navbar/Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -126,7 +126,7 @@ function Navbar({ photo, displayName }) {
       {/* 2 */}
       <div className="flex items-center justify-center gap-1 select-none w-full max-w-[100px] mx-auto relative bg-yellow-100">
         {/* info */}
-        <div className="w-[210px] h-[200px] bg-white ring-1 absolute top-0 mx-auto"></div>
+        <div className="w-[210px] h-[200px] bg-white/30 ring-1 absolute top-2 mx-auto"></div>
         <div className="w-7 min-w-7">
           <img src="/logo.png" alt="logo" className="w-full h-fit " />
         </div>
