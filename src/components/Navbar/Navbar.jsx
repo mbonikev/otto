a859fe9d7f598a4ebc17d41113975d5667dd3a8d
mbonikev <mbonimpayekevin@gmail.com>
2025-02-03 16:39:43 +0200

    updated src/components/Navbar/Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -131,7 +131,7 @@ function Navbar({ photo, displayName }) {
             <h1 className="font-Kanit delay-150 opacity-0 group-hover:opacity-100 text-2xl font-light text-dark-text-weak text-center">
               Otto
             </h1>
-            <h1 className="text-sm delay-150 opacity-0 group-hover:opacity-100 font-light text-dark-text-weak mt-1">
+            <h1 className="text-sm delay-200 opacity-0 group-hover:opacity-100 font-light text-dark-text-weak mt-1">
               V1.0.1
             </h1>
           </div>
