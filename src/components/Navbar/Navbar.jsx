7a4f9676f9d9d98450b442b61f74b876c7565df3
mbonikev <mbonimpayekevin@gmail.com>
2025-02-03 17:16:07 +0200

    updated src/components/Navbar/Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -147,7 +147,7 @@ function Navbar({ photo, displayName }) {
                   </div>
                   <div className="flex flex-col gap-[2px] items-start leading-5">
                     <h1>Otto Pro</h1>
-                    <p className="text-xs text-dark-text/50 leading-3">
+                    <p className="text-[11px] text-dark-text/50 leading-3">
                       Upgrade to{" "}
                     </p>
                   </div>
