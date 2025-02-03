6170d4a0d7bfcfec64d7ed4aebe211d24f02c6ec
mbonikev <mbonimpayekevin@gmail.com>
2025-02-03 16:42:23 +0200

    updated src/components/Navbar/Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -126,7 +126,7 @@ function Navbar({ photo, displayName }) {
       {/* 2 */}
       <div className="flex items-center justify-center gap-1 select-none w-full max-w-[100px] mx-auto relative group bg-yellow-100">
         {/* info */}
-        <div className="w-0 h-0 group-hover:w-[210px] delay-150 group-hover:h-[185px] opacity-0 group-hover:opacity-100 transition-all bg-transparent absolute top-0 mx-auto z-10 pt-2">
+        <div className="w-[210px] h-0 delay-150 group-hover:h-[185px] opacity-0 group-hover:opacity-100 transition-all bg-transparent absolute top-0 mx-auto z-10 pt-2">
           <div className="w-full h-full bg-white ring-1 ring-stone-200 shadow-lg rounded-3xl pt-[88px] px-2 pb-2 text-center">
             <h1 className="font-Kanit delay-300 transition-all whitespace-nowrap opacity-0 group-hover:opacity-100 text-2xl font-light text-dark-text-weak text-center">
               Otto
