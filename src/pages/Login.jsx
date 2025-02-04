c059379e222c71e064bdfb489c5f2f9500d3d9bd
mbonikev <mbonimpayekevin@gmail.com>
2025-02-04 13:49:27 +0200

    updated src/pages/Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,8 +5,14 @@ const Login = () => {
     <div className="h-screen flex justify-start items-center flex-col p-5">
       <div className="w-full h-fit flex">
         <img src="./logo.png" className="h-9" />
+        <h1 className="font-Kanit delay-150 transition-all whitespace-nowrap opacity-0 group-hover:opacity-100 text-2xl font-light text-dark-text-weak text-center">
+          Otto
+        </h1>
       </div>
-      <a href={`${backendUrl}/auth/google`} className="bg-blue-500 text-white p-3 rounded">
+      <a
+        href={`${backendUrl}/auth/google`}
+        className="bg-blue-500 text-white p-3 rounded"
+      >
         Continue with Google
       </a>
     </div>
