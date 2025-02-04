dc3dca2dfc6b343c728ab2ae293355ba6be5d4cc
mbonikev <mbonimpayekevin@gmail.com>
2025-02-04 11:47:40 +0200

    updated src/components/ProtectedRoutes/ProtectedRoutes.jsx

--- src/components/ProtectedRoutes/ProtectedRoutes.jsx
+++ src/components/ProtectedRoutes/ProtectedRoutes.jsx
@@ -6,6 +6,7 @@ const ProtectedRoutes = () => {
   const apiUrl = import.meta.env.VITE_BACKEND_API;
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [models, setModels] = useState("")
 
   useEffect(() => {
     const fetchUserStatus = async () => {
@@ -26,7 +27,19 @@ const ProtectedRoutes = () => {
       }
     };
 
-    const fetchModels = async () => {};
+    const fetchModels = async () => {
+      try {
+        const response = await axios.get(`${apiUrl}/auth/status`, {
+          withCredentials: true, // Sends JWT token in cookies
+        });
+
+        setUser(response.data.user || null);
+        // console.log(response.data);
+      } catch (error) {
+        console.error("Error fetching user status:", error);
+        setUser(null);
+      }
+    };
 
     fetchUserStatus();
   }, []);
