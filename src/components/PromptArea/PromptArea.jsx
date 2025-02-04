6de00180ff2c55abb46042f105717b38c9a42571
mbonikev <mbonimpayekevin@gmail.com>
2025-02-04 11:34:39 +0200

    updated src/components/PromptArea/PromptArea.jsx

--- src/components/PromptArea/PromptArea.jsx
+++ src/components/PromptArea/PromptArea.jsx
@@ -119,7 +119,7 @@ function PromptArea({
             <div className="group cursor-default h-full w-fit px-3 flex items-center justify-center gap-1 text-sm ring-1 ring-stone-300 text-dark-text-weak font-semibold rounded-full relative">
               <BsFileEarmarkText className="text-base text-blue-400 stroke-[0.6px]" />
               Summarize
-              <Tooltip title="comming soon" placement="right-center" />
+              <Tooltip title="Comming Soon" placement="right-center" />
             </div>
           </div>
           {/* 2 */}
