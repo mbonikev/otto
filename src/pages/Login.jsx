a6c8adccfecf896b0d9ccfde8463452d04164b4e
mbonikev <mbonimpayekevin@gmail.com>
2025-02-04 13:48:37 +0200

    updated src/pages/Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,8 +3,8 @@ const Login = () => {
 
   return (
     <div className="h-screen flex justify-start items-center flex-col">
-      <div className="w-full h-fit flex p-2 bg-red-300">
-        <img src="./logo.png" className="h-12" />
+      <div className="w-full h-fit flex p-2">
+        <img src="./logo.png" className="h-9" />
       </div>
       <a href={`${backendUrl}/auth/google`} className="bg-blue-500 text-white p-3 rounded">
         Continue with Google
