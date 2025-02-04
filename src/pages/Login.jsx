fbbad40a8e4f686a104a52bb0d468948e257e176
mbonikev <mbonimpayekevin@gmail.com>
2025-02-04 13:48:47 +0200

    updated src/pages/Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,8 +2,8 @@ const Login = () => {
   const backendUrl = import.meta.env.VITE_BACKEND_API;
 
   return (
-    <div className="h-screen flex justify-start items-center flex-col">
-      <div className="w-full h-fit flex p-2">
+    <div className="h-screen flex justify-start items-center flex-col p-5">
+      <div className="w-full h-fit flex">
         <img src="./logo.png" className="h-9" />
       </div>
       <a href={`${backendUrl}/auth/google`} className="bg-blue-500 text-white p-3 rounded">
