21203c7227220f891172e55683a7bf76d813757e
mbonikev <mbonimpayekevin@gmail.com>
2025-02-04 11:20:58 +0200

    updated src/pages/Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -108,7 +108,7 @@ function Home() {
                 return (
                   <div
                     key={index}
-                    className={`message text-base font-normal text-dark-text leading-[26px] overflow-x-auto no_scroll whitespace-pre-wrap ${
+                    className={`message text-base font-normal text-dark-text leading-[26px] overflow-x-hidden no_scroll whitespace-pre-wrap ${
                       msg.role === "user"
                         ? "w-fit bg-stone-200/40 px-4 py-2 ml-auto max-w-[600px] rounded-3xl"
                         : "w-full bg-transparent flex items-start justify-start gap-4 break-words whitespace-pre-wrap pr-9"
