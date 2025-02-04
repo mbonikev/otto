cf0aedd0f68fbc3ad56ed1154bb05c5874611b3c
mbonikev <mbonimpayekevin@gmail.com>
2025-02-04 12:02:50 +0200

    updated src/components/PromptArea/PromptArea.jsx

--- src/components/PromptArea/PromptArea.jsx
+++ src/components/PromptArea/PromptArea.jsx
@@ -17,7 +17,7 @@ function PromptArea({
   setThinking,
   displayName,
   email,
-  models
+  models,
 }) {
   const [message, setMessage] = useState("");
   const textareaRef = useRef(null);
@@ -108,17 +108,15 @@ function PromptArea({
         <div className="flex items-center justify-between w-full h-[36px] bg-red-400/0 min-h-[32px] px-1.5 mb-1.5">
           {/* 1 */}
           <div className="w-full h-full flex items-center justify-start gap-3 select-none">
-            <div className="group cursor-pointer h-full w-fit px-3 flex items-center justify-center gap-1 text-sm ring-1 ring-stone-300 text-dark-text-weak hover:text-dark-text font-semibold rounded-full relative">
-              <div className="w-full h-full flex items-center justify-center gap-1"></div>
-              <LuBrain className="text-lg text-purple-500" />
-              llama3-8b-8192
-              <HiChevronUpDown className="text-lg opacity-75" />
-              <Tooltip title="Models" placement="right-center" />
-
+            <div className=" cursor-pointer h-full w-fit px-3 flex items-center justify-center gap-1 text-sm ring-1 ring-stone-300 text-dark-text-weak hover:text-dark-text font-semibold rounded-full relative">
+              <div className="group w-full h-full flex items-center justify-center gap-1">
+                <LuBrain className="text-lg text-purple-500" />
+                llama3-8b-8192
+                <HiChevronUpDown className="text-lg opacity-75" />
+                <Tooltip title="Models" placement="right-center" />
+              </div>
               {/* models */}
               <div className="w-[300px] h-[300px] bg-white ring-1 ring-stone-200 absolute mx-auto bottom-[130%] "></div>
-
-
             </div>
             <div className="group cursor-pointer h-full w-fit px-3 flex items-center justify-center gap-1 text-sm ring-1 ring-stone-300 text-dark-text-weak hover:text-dark-text font-semibold rounded-full relative">
               <GoCommandPalette className="text-lg text-lime-600 stroke-[1px]" />
