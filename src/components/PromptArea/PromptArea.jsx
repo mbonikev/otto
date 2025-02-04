861d630b528b175a55eac6b31047b47500d399cb
mbonikev <mbonimpayekevin@gmail.com>
2025-02-04 12:00:40 +0200

    updated src/components/PromptArea/PromptArea.jsx

--- src/components/PromptArea/PromptArea.jsx
+++ src/components/PromptArea/PromptArea.jsx
@@ -84,7 +84,7 @@ function PromptArea({
     <div className="w-full h-fit flex flex-col gap-3 px-3 pb-3 z-0 sticky bottom-0 bg-white">
       <form
         onSubmit={submit}
-        className="w-full max-w-[850px] mx-auto h-fit rounded-[25px] shadow-lg ring-1 p-2 ring-stone-200 overflow-hidden flex flex-col gap-5"
+        className="w-full max-w-[850px] mx-auto h-fit rounded-[25px] shadow-lg ring-1 p-2 ring-stone-200 flex flex-col gap-5"
       >
         <textarea
           type="text"
