0b0105fdb0f79657cd01dedbc853bc3e34f9d847
mbonikev <mbonimpayekevin@gmail.com>
2025-02-04 11:59:30 +0200

    updated src/components/PromptArea/PromptArea.jsx

--- src/components/PromptArea/PromptArea.jsx
+++ src/components/PromptArea/PromptArea.jsx
@@ -115,7 +115,8 @@ function PromptArea({
               <Tooltip title="Models" placement="right-center" />
 
               {/* models */}
-              <div className="w-[400px]"></div>
+              <div className="w-[400px] h-[200px] absp
+              "></div>
             </div>
             <div className="group cursor-pointer h-full w-fit px-3 flex items-center justify-center gap-1 text-sm ring-1 ring-stone-300 text-dark-text-weak hover:text-dark-text font-semibold rounded-full relative">
               <GoCommandPalette className="text-lg text-lime-600 stroke-[1px]" />
