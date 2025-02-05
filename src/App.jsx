a21112fd4a5f5db6dde9e5facbd778064241fadd
mbonikev <mbonimpayekevin@gmail.com>
2025-02-05 15:59:32 +0200

    updated src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,31 +3,22 @@ import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 import Login from "./pages/Login";
+import ProtectedRoutes from "./components/ProtectedRoutes/ProtectedRoutes";
 import Chat from "./pages/Chat";
-import LoggedInRoutes from "./components/ProtectedRoutes/LoggedInRoutes";
-import LoggedOutRoutes from "./components/ProtectedRoutes/LoggedOutRoutes";
 
 function App() {
   const hasHash = window.location.hash.includes("#");
   if (!hasHash && window.location.pathname !== "/") {
     return <NotFound />;
   }
-
   return (
     <HashRouter>
       <Routes>
-        {/* Protect Home and Login routes for logged-out users */}
-        <Route element={<LoggedOutRoutes />}>
+        <Route element={<ProtectedRoutes />}>
           <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-        </Route>
-
-        {/* Protect Chat routes for logged-in users */}
-        <Route element={<LoggedInRoutes />}>
           <Route path="/c/:id" element={<Chat />} />
         </Route>
-
-        {/* Handle Not Found */}
+        <Route path="/login" element={<Login />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </HashRouter>
