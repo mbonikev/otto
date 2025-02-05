40f25ee97db1d3918c2f4050f69aca844df3804f
mbonikev <mbonimpayekevin@gmail.com>
2025-02-05 16:03:02 +0200

    updated src/pages/Chat.jsx

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -91,7 +91,7 @@ function Chat() {
   return (
     <div className="w-full h-svh flex flex-col relative">
       <div className="w-full h-fit z-30 fixed top-0">
-        <Navbar photo={photo} displayName={displayName} user={user}/>
+        <Navbar photo={photo} displayName={displayName}/>
       </div>
 
       <div
@@ -147,10 +147,6 @@ function Chat() {
         setMessages={setMessages}
         thinking={thinking}
         setThinking={setThinking}
-        displayName={displayName}
-        email={email}
-        models={models}
-        user={user}
       />
     </div>
   );
