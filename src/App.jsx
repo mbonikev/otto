359ec5cb65c3553bd7c0fd83b6dcc0d142015d4c
mbonikev <mbonimpayekevin@gmail.com>
2025-02-05 15:56:02 +0200

    updated src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,9 @@ import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 import Login from "./pages/Login";
-import ProtectedRoutes from "./components/ProtectedRoutes/ProtectedRoutes";
 import Chat from "./pages/Chat";
+import LoggedInRoutes from "./components/LoggedInRoutes";
+import LoggedOutRoutes from "./components/LoggedOutRoutes";
 
 function App() {
   const hasHash = window.location.hash.includes("#");
@@ -15,14 +16,14 @@ function App() {
   return (
     <HashRouter>
       <Routes>
-        {/* Protect Home and Login routes for logged-in users */}
-        <Route element={<ProtectedRoutes />}>
+        {/* Protect Home and Login routes for logged-out users */}
+        <Route element={<LoggedOutRoutes />}>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
         </Route>
 
         {/* Protect Chat routes for logged-in users */}
-        <Route element={<ProtectedRoutes />}>
+        <Route element={<LoggedInRoutes />}>
           <Route path="/c/:id" element={<Chat />} />
         </Route>
 
