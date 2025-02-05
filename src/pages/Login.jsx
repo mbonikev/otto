13016ea3c4365e9e5486d24fb27f37c9010ad927
mbonikev <mbonimpayekevin@gmail.com>
2025-02-05 12:50:27 +0200

    updated src/pages/Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,7 +4,7 @@ const Login = () => {
   return (
     <div className="h-screen flex justify-start items-center flex-col p-5">
       <div className="w-full h-full grid grid-cols-2 max-lg:grid-cols-1">
-        <div className="h-full flex-1 flex flex-col items-center justify-start gap-6 w-full py-14">
+        <div className="h-full flex-1 flex flex-col items-center justify-start gap-6 w-full">
           <div className="w-full h-fit flex gap-2">
             <img src="./logo.png" className="h-9" />
             <h1 className="font-Kanit text-2xl font-light text-dark-text-weak text-center">
