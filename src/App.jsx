2ba04a1b4c492e861fb8a7390db653ea060b58f8
mbonikev <mbonimpayekevin@gmail.com>
2025-02-05 15:39:21 +0200

    updated src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,7 @@ function App() {
         {/* Protect login-related routes for non-logged-in users */}
         <Route element={<ProtectedRoutesLoggedIn />}>
           <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
         </Route>
 
         {/* Protect chat-related routes for logged-in users */}
@@ -28,8 +29,6 @@ function App() {
 
         {/* Catch-all route */}
         <Route path="*" element={<NotFound />} />
-        <Route path="/login" element={<Login />} />
-
       </Routes>
     </HashRouter>
   );
