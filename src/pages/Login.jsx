f843251eb30cb956ef67ad4f8f15b95831fd2887
mbonikev <mbonimpayekevin@gmail.com>
2025-02-05 12:49:56 +0200

    updated src/pages/Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,6 +5,12 @@ const Login = () => {
     <div className="h-screen flex justify-start items-center flex-col p-5">
       <div className="w-full h-full grid grid-cols-2 max-lg:grid-cols-1">
         <div className="h-full flex-1 flex flex-col items-center justify-center gap-6 w-full py-14">
+          <div className="w-full h-fit flex gap-2">
+            <img src="./logo.png" className="h-9" />
+            <h1 className="font-Kanit text-2xl font-light text-dark-text-weak text-center">
+              Otto
+            </h1>
+          </div>
           <h1 className="text-3xl font-semibold">Welcome back</h1>
           <a
             href={`${backendUrl}/auth/google`}
