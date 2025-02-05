54a922fcc791441e71ba3ba18e44d711f6169d78
mbonikev <mbonimpayekevin@gmail.com>
2025-02-05 14:38:56 +0200

    updated src/components/Navbar/Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -272,11 +272,17 @@ function Navbar({ photo, displayName, user }) {
           </>
         ) : (
           <>
-            <button className="group h-10 w-auto aspect-square flex items-center justify-center text-2xl hover:bg-stone-100 text-dark-text-weak hover:text-dark-text rounded-full relative">
+            <button
+              onClick={() => (window.location = "/#/login")}
+              className="group h-10 w-auto aspect-square flex items-center justify-center text-2xl hover:bg-stone-100 text-dark-text-weak hover:text-dark-text rounded-full relative"
+            >
               <BsIncognito />
               <Tooltip title="Temporary Mode" placement="center" />
             </button>
-            <button className="group h-10 w-auto aspect-square flex items-center justify-center text-2xl hover:bg-stone-100 text-dark-text-weak hover:text-dark-text rounded-full relative">
+            <button
+              onClick={() => (window.location = "/#/login")}
+              className="group h-10 w-auto aspect-square flex items-center justify-center text-2xl hover:bg-stone-100 text-dark-text-weak hover:text-dark-text rounded-full relative"
+            >
               <IoShareSocialOutline />
               <Tooltip title="Share" placement="center" />
             </button>
