026ff6c32076223212421119d71a3238ef7e9c4c
mbonikev <mbonimpayekevin@gmail.com>
2025-02-05 23:36:24 +0200

    updated src/pages/Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -67,36 +67,37 @@ function Home() {
       const apiUrl = import.meta.env.VITE_BACKEND_API;
       const apiKey = import.meta.env.VITE_GROQ_API_KEY;
       setThinking(true);
-      try {
-        const response = await axios.get(`${apiUrl}/api/getmsg`, {
-          params: { convId: retrieveId },
-          headers: {
-            Authorization: `Bearer ${apiKey}`,
-            "Content-Type": "application/json",
-          },
-        });
-        if (response.data) {
-          const mappedMessages = response.data.flatMap((msg) => [
-            {
-              role: "user",
-              content: msg.content[0]?.prompt, // User's message
-              title: msg.title || "Untitled",
-              convId: msg.conversationId,
+      if (retrieveId !== "") {
+        try {
+          const response = await axios.get(`${apiUrl}/api/getmsg`, {
+            params: { convId: retrieveId },
+            headers: {
+              Authorization: `Bearer ${apiKey}`,
+              "Content-Type": "application/json",
             },
-            {
-              role: "assistant",
-              content: msg.content[0]?.reply, // Assistant's reply
-              title: msg.title || "Untitled",
-              convId: msg.conversationId,
-            },
-          ]);
-          setMessages(mappedMessages);
+          });
+          if (response.data) {
+            const mappedMessages = response.data.flatMap((msg) => [
+              {
+                role: "user",
+                content: msg.content[0]?.prompt, // User's message
+                title: msg.title || "Untitled",
+                convId: msg.conversationId,
+              },
+              {
+                role: "assistant",
+                content: msg.content[0]?.reply, // Assistant's reply
+                title: msg.title || "Untitled",
+                convId: msg.conversationId,
+              },
+            ]);
+            setMessages(mappedMessages);
+            setThinking(false);
+          }
+        } catch (error) {
+          console.error("messages:", error);
           setThinking(false);
         }
-        
-      } catch (error) {
-        console.error("messages:", error);
-        setThinking(false);
       }
     };
 
