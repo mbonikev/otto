d22ab3e4b508b96910c2562f89f0717bb69056ec
mbonikev <mbonimpayekevin@gmail.com>
2025-02-05 23:21:03 +0200

    updated src/pages/Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -69,7 +69,7 @@ function Home() {
       try {
         setThinking(true);
         const response = await axios.get(`${apiUrl}/api/getmsg`, {
-          params: { convId: retrieveId, userId },
+          params: { convId: retrieveId },
           headers: {
             Authorization: `Bearer ${apiKey}`,
             "Content-Type": "application/json",
