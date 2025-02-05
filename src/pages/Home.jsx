34da8b36c15ea5ed82c7f03f10bd791f13a5f534
mbonikev <mbonimpayekevin@gmail.com>
2025-02-06 00:03:57 +0200

    updated src/pages/Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -125,7 +125,7 @@ function Home() {
     // Call both functions to fetch data
     handleGetConvs();
     handleGetMessages();
-  }, [messages]);
+  }, []);
 
   // Function to render content with code blocks
   const renderContent = (msg) => {
