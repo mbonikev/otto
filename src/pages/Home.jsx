52c6942e6d98d73c7023d30eb47345234874a92d
mbonikev <mbonimpayekevin@gmail.com>
2025-02-05 15:17:29 +0200

    updated src/pages/Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -92,7 +92,7 @@ function Home() {
   return (
     <div className="w-full h-svh flex flex-col relative">
       <div className="w-full h-fit z-30 fixed top-0">
-        <Navbar user={user} />
+        <Navbar />
       </div>
 
       <div
