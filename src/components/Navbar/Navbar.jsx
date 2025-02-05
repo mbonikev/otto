adf36c2b26e811051485e08bd5a03aa94a757b12
mbonikev <mbonimpayekevin@gmail.com>
2025-02-05 16:49:45 +0200

    updated src/components/Navbar/Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -19,10 +19,11 @@ import { BiSolidLockAlt } from "react-icons/bi";
 import Cookies from "js-cookie";
 import avatar3 from "/avatars/avatar3.png";
 
-function Navbar({ photo, displayName, user, chatsModal, setChatsModal }) {
+function Navbar({ photo, displayName, user }) {
   const [showPopup, setShowPopup] = useState(false);
   const profileRef = useRef(null);
   const popupRef = useRef(null);
+  const [chatsModal, setChatsModal] = useState(false);
   const [animateChatsModal, setAnimateChatsModal] = useState(false);
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
