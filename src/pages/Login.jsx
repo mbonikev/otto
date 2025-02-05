c4c3f9a4a423ecb944e8b40150e0e02421d046dd
mbonikev <mbonimpayekevin@gmail.com>
2025-02-05 12:49:46 +0200

    updated src/pages/Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,12 +3,6 @@ const Login = () => {
   document.title = "Login - Otto";
   return (
     <div className="h-screen flex justify-start items-center flex-col p-5">
-      <div className="w-full h-fit flex gap-2">
-        <img src="./logo.png" className="h-9" />
-        <h1 className="font-Kanit text-2xl font-light text-dark-text-weak text-center">
-          Otto
-        </h1>
-      </div>
       <div className="w-full h-full grid grid-cols-2 max-lg:grid-cols-1">
         <div className="h-full flex-1 flex flex-col items-center justify-center gap-6 w-full py-14">
           <h1 className="text-3xl font-semibold">Welcome back</h1>
