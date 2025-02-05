c343c4e28786e59b3a3a91fc5fc122925c0358b6
mbonikev <mbonimpayekevin@gmail.com>
2025-02-05 14:52:47 +0200

    updated src/components/ProtectedRoutes/ProtectedRoutesLoggedIn.jsx

--- src/components/ProtectedRoutes/ProtectedRoutesLoggedIn.jsx
+++ src/components/ProtectedRoutes/ProtectedRoutesLoggedIn.jsx
@@ -4,7 +4,7 @@ import axios from "axios";
 import Cookies from "js-cookie"; // Install via `npm install js-cookie`
 
 const ProtectedRoutesLoggedIn = () => {
-  const loggedIn = Cookies.get("selectedModel");
+  const loggedIn = Cookies.get("token");
 
   return loggedIn ? (
     <Outlet />
