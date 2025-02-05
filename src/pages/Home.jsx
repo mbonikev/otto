11124a70bccc6554ab8f279439d1a910ac710585
mbonikev <mbonimpayekevin@gmail.com>
2025-02-05 23:28:23 +0200

    updated src/pages/Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -106,7 +106,7 @@ function Home() {
       const apiUrl = import.meta.env.VITE_BACKEND_API;
       const apiKey = import.meta.env.VITE_GROQ_API_KEY;
       try {
-        setThinking(true);
+        // setThinking(true);
         const response = await axios.get(`${apiUrl}/api/getconvs`, {
           params: { userId },
           headers: {
@@ -120,9 +120,9 @@ function Home() {
         }
       } catch (error) {
         console.error("conversations:", error);
-        setThinking(false);
+        // setThinking(false);
       } finally {
-        setThinking(false);
+        // setThinking(false);
       }
     };
 
