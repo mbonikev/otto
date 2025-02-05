853dc01efb6b85bdcb7517580400f230f7f28742
mbonikev <mbonimpayekevin@gmail.com>
2025-02-05 15:24:10 +0200

    updated src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,31 +3,22 @@ import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 import Login from "./pages/Login";
-import ProtectedRoutes from "./components/ProtectedRoutes/ProtectedRoutes";
 import Chat from "./pages/Chat";
 import ProtectedRoutesLoggedIn from "./components/ProtectedRoutes/ProtectedRoutesLoggedIn";
 
 function App() {
-  const hasHash = window.location.hash.includes("#");
-  if (!hasHash && window.location.pathname !== "/") {
-    return <NotFound />;
-  }
-
   return (
     <HashRouter>
       <Routes>
-        {/* Protect login-related routes for non-logged-in users */}
+        {/* Wrap the routes that should be protected */}
         <Route element={<ProtectedRoutesLoggedIn />}>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
         </Route>
 
-        {/* Protect chat-related routes for logged-in users */}
-        <Route element={<ProtectedRoutes />}>
-          <Route path="/c/:id" element={<Chat />} />
-        </Route>
-
-        {/* Catch-all route */}
+        {/* Regular routes for logged-in users */}
+        <Route path="/c/:id" element={<Chat />} />
+        
         <Route path="*" element={<NotFound />} />
       </Routes>
     </HashRouter>
