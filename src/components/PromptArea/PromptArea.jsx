2d20ab6aed275713779e196b2feb9ddb912b1541
mbonikev <mbonimpayekevin@gmail.com>
2025-02-05 18:53:30 +0200

    updated src/components/PromptArea/PromptArea.jsx

--- src/components/PromptArea/PromptArea.jsx
+++ src/components/PromptArea/PromptArea.jsx
@@ -37,7 +37,7 @@ function PromptArea({
   );
   const [searchModel, setSearchModel] = useState("");
   const { chat } = useParams();
-  const [convId, setConvId] = useState(Cookies.get("convId") || chat);
+  const [convId, setConvId] = useState("");
   const navigate = useNavigate();
 
   useEffect(() => {
