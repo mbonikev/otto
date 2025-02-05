1c34aa2a8c05463147448e1ce06c88e8d9ae424e
mbonikev <mbonimpayekevin@gmail.com>
2025-02-05 20:56:10 +0200

    updated src/pages/Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -67,6 +67,7 @@ function Home() {
         const apiUrl = import.meta.env.VITE_BACKEND_API;
         const apiKey = import.meta.env.VITE_GROQ_API_KEY;
         try {
+          setThinking(true);
           const response = await axios.get(`${apiUrl}/api/getconvs`, {
             params: { convId: retrieveId },
             headers: {
