3d601ff1faf55f275ae792b68f1f41280b29586a
mbonikev <mbonimpayekevin@gmail.com>
2025-02-06 00:29:09 +0200

    updated src/components/PromptArea/PromptArea.jsx

--- src/components/PromptArea/PromptArea.jsx
+++ src/components/PromptArea/PromptArea.jsx
@@ -15,7 +15,7 @@ import {
   HiMiniXCircle,
 } from "react-icons/hi2";
 import Cookies from "js-cookie";
-import { useNavigate, useParams } from "react-router-dom";
+import { useLocation, useNavigate, useParams } from "react-router-dom";
 
 function PromptArea({
   setMessages,
@@ -37,9 +37,12 @@ function PromptArea({
     Cookies.get("selectedModel") || "llama3-8b-8192"
   );
   const [searchModel, setSearchModel] = useState("");
-  const { chat } = useParams();
+  const [chatId, setChatId] = useState(null);
   const [convId, setConvId] = useState("");
   const navigate = useNavigate();
+  const location = useLocation(); // Use the useLocation hook to get the current URL
+
+  // Get the 'chat' parameter from the URL
 
   useEffect(() => {
     const retrieveId = Cookies.get("convId") ?? chat;
@@ -47,7 +50,9 @@ function PromptArea({
     if (retrieveId) {
       navigate(`?chat=${retrieveId}`, { replace: true });
     }
-  }, [convId]);
+    const urlParams = new URLSearchParams(location.search);
+    setChatId(urlParams.get("chat"));
+  }, [chatId]);
 
   const handleInput2Change = (e) => {
     const newInput = e.target.value;
@@ -84,7 +89,7 @@ function PromptArea({
             userId,
             selectedModel,
             convId,
-            messages
+            messages,
           },
           {
             headers: {
@@ -103,7 +108,7 @@ function PromptArea({
           });
         }
         const { title, message: assistantResponse } = response.data;
-        
+
         setThinking(false);
 
         // Add assistant's response with title to the state
