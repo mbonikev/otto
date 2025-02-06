65249da5aa3a9091004b991a1c1bfb92b716b726
mbonikev <mbonimpayekevin@gmail.com>
2025-02-06 08:08:56 +0200

    updated src/pages/Chat.jsx

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -108,7 +108,7 @@ function Chat() {
                 return (
                   <div
                     key={index}
-                    className={`message text-base font-normal text-dark-text leading-[26px] overflow-x-auto no_scroll whitespace-pre-wrap p-1 ${
+                    className={`message text-base font-normal text-dark-text dark:text-light-color leading-[26px] overflow-x-auto no_scroll whitespace-pre-wrap p-1 ${
                       msg.role === "user"
                         ? "w-fit bg-stone-200/40 px-4 py-2 ml-auto max-w-[600px] rounded-3xl"
                         : "w-full bg-transparent flex items-start justify-start gap-4 break-words whitespace-pre-wrap pr-9"
