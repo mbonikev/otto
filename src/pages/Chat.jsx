f3076588f7e575d07826d1636071107e0419c149
mbonikev <mbonimpayekevin@gmail.com>
2025-02-06 08:32:18 +0200

    updated src/pages/Chat.jsx

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -67,7 +67,7 @@ function Chat() {
                 }}
                 wrapLines={true}
                 language="javascript"
-                style={oneLight}
+                style={isDarkMode ? oneDark : oneLight} // Switch theme based on dark mode
                 className={`rounded-2xl overflow-x-auto max-w-[100%] p-6 break-all whitespace-pre-wrap`}
               >
                 {part}
