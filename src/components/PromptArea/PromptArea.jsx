6fc65803177b02416f63ad230116f62ec8c37f75
mbonikev <mbonimpayekevin@gmail.com>
2025-02-06 02:13:07 +0200

    updated src/components/PromptArea/PromptArea.jsx

--- src/components/PromptArea/PromptArea.jsx
+++ src/components/PromptArea/PromptArea.jsx
@@ -232,7 +232,7 @@ function PromptArea({
                       type="text"
                       autoFocus={true}
                       onChange={(e) => setSearchModel(e.target.value)}
-                      className="h-full w-full bg-transparent text-dark-text placeholder:text-dark-text-weak/50 text-sm font-normal"
+                      className="h-full w-full bg-transparent text-dark-text dark:text-light-color placeholder:text-dark-text-weak/50 text-sm font-normal"
                       placeholder="Search models..."
                     />
                   </div>
