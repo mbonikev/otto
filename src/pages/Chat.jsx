2310a2be9bfec872d244e1d21cb88a406f258a48
mbonikev <mbonimpayekevin@gmail.com>
2025-02-06 08:44:59 +0200

    updated src/pages/Chat.jsx

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -131,7 +131,7 @@ function Chat() {
                     key={index}
                     className={`message text-base font-normal text-dark-text dark:text-light-color leading-[26px] overflow-x-auto no_scroll whitespace-pre-wrap flex p-1 ${
                       msg.role === "user"
-                        ? ""
+                        ? "w-full bg-stone-200/40 dark:bg-card-dark-1 px-6 py-4 ml-auto max-w-[600px] rounded-3xl break-words whitespace-pre-wrap flex items-start justify-start"
                         : "w-full bg-transparent flex items-start justify-start gap-4 break-words whitespace-pre-wrap pr-9"
                     } ${isLastNonUserMsg ? "animate-message" : ""}`}
                   >
