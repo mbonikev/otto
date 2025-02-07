716864e80f56fd730289a027d10d26c88f00bd89
mbonikev <mbonimpayekevin@gmail.com>
2025-02-07 21:13:31 +0200

    updated src/pages/Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -9,34 +9,31 @@ const Login = () => {
   document.title = "Login - Otto";
   return (
     <div className="h-svh flex justify-start items-start flex-col max-lg:p-0">
-      <div className="w-full h-full flex">
-        <div className="h-full flex-1 flex flex-col items-start justify-start gap-6 w-full p-6">
-          <Link to={"/"} className="w-fit h-fit flex gap-2">
-            <img src="./logo.png" className="h-9" />
-            <h1 className="font-Kanit text-2xl font-light text-dark-text-weak dark:text-light-color-weak text-center">
-              Otto
-            </h1>
-          </Link>
-          <div className="w-full flex-1 flex flex-col items-center justify-center gap-3">
-            <h1 className="text-3xl font-semibold max-2xl:text-2xl mb-4 text-dark-text dark:text-light-color">
-              Welcome back
-            </h1>
-            <a
-              href={`${backendUrl}/auth/google`}
-              className="bg-white dark:bg-body-dark hover:bg-stone-200 dark:hover:bg-card-hover-dark ring-1 ring-stone-200 dark:ring-card-hover-dark text-dark-text dark:text-light-color py-2 px-3 w-fit min-w-[230px] max-md:min-w-fit max-md:pr-5 max-lg:text-sm flex items-center justify-start gap-3 rounded-full"
-            >
-              <FcGoogle className="text-3xl" />
-              Continue with Google
-            </a>
-          </div>
-          <div className="w-full flex items-center justify-between p-1 flex-wrap gap-3 max-lg:justify-center">
-            <h1 className="text-dark-text-weak dark:text-light-color-weak text-sm font-medium">
-              &copy; Otto 2025. All rights reserved.
-            </h1>
-            <div className="flex items-center justify-end gap-3 text-dark-text-weak dark:text-light-color-weak text-sm">
-              <Link to="/">Terms of Use</Link>|
-              <Link to="/">Privacy Policy</Link>
-            </div>
+      <div className="h-full flex-1 flex flex-col items-start justify-start gap-6 w-full p-6">
+        <Link to={"/"} className="w-fit h-fit flex gap-2">
+          <img src="./logo.png" className="h-9" />
+          <h1 className="font-Kanit text-2xl font-light text-dark-text-weak dark:text-light-color-weak text-center">
+            Otto
+          </h1>
+        </Link>
+        <div className="w-full flex-1 flex flex-col items-center justify-center gap-3">
+          <h1 className="text-3xl font-semibold max-2xl:text-2xl mb-4 text-dark-text dark:text-light-color">
+            Welcome back
+          </h1>
+          <a
+            href={`${backendUrl}/auth/google`}
+            className="bg-white dark:bg-body-dark hover:bg-stone-200 dark:hover:bg-card-hover-dark ring-1 ring-stone-200 dark:ring-card-hover-dark text-dark-text dark:text-light-color py-2 px-3 w-fit min-w-[230px] max-md:min-w-fit max-md:pr-5 max-lg:text-sm flex items-center justify-start gap-3 rounded-full"
+          >
+            <FcGoogle className="text-3xl" />
+            Continue with Google
+          </a>
+        </div>
+        <div className="w-full flex items-center justify-between p-1 flex-wrap gap-3 max-lg:justify-center">
+          <h1 className="text-dark-text-weak dark:text-light-color-weak text-sm font-medium">
+            &copy; Otto 2025. All rights reserved.
+          </h1>
+          <div className="flex items-center justify-end gap-3 text-dark-text-weak dark:text-light-color-weak text-sm">
+            <Link to="/">Terms of Use</Link>|<Link to="/">Privacy Policy</Link>
           </div>
         </div>
       </div>
