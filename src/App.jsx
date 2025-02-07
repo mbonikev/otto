c031ceb4387cd6c70fb7f9fe8f79d9fba1d0ee21
mbonikev <mbonimpayekevin@gmail.com>
2025-02-07 14:43:53 +0200

    updated src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -31,7 +31,7 @@ function App() {
       {loginModal && (
         <div
           onClick={handleCloseLoginModal}
-          className={`fixed top-0 left-0 w-full h-svh bg-black/30 dark:bg-black/60 z-30 transition-opacity duration-300 ${
+          className={`fixed top-0 left-0 w-full h-svh bg-black/15 dark:bg-black/30 z-30 transition-opacity duration-300 ${
             animateLoginModal ? "opacity-100" : "opacity-0"
           }`}
         />
