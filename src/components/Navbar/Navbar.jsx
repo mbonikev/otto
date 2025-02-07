976a14c3f1cd46259854558cadf1a2fc270dfb32
mbonikev <mbonimpayekevin@gmail.com>
2025-02-07 13:25:01 +0200

    updated src/components/Navbar/Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -171,7 +171,7 @@ function Navbar({
           onClick={user ? handleOpenChatsModal : handleLoginModal}
           className="group h-10 w-auto aspect-square flex items-center justify-center text-[22px] hover:bg-stone-100 text-dark-text-weak dark:text-light-color-weak hover:text-dark-text dark:hover:bg-card-hover-dark dark:hover:text-light-color-weak rounded-full relative"
         >
-          <HiOutlineChatBubbleLeftRight className="stroke-[2px]" />
+          <HiOutlineChatBubbleLeftRight className="stroke-[2.5px]" />
           <Tooltip title="Chat history" placement="left" shortcut="Alt + c" />
         </button>
         <button
