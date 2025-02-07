48f63cce9fad9096a09b2a77f5ce2d936ceb35f5
mbonikev <mbonimpayekevin@gmail.com>
2025-02-07 21:52:15 +0200

    updated src/pages/Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -18,8 +18,8 @@ const Login = () => {
             </h1>
           </Link>
           <div className="w-full flex-1 flex flex-col items-start justify-start gap-3 pt-[5vh]">
-            <h1 className="text-3xl font-semibold max-2xl:text-2xl mb-4 text-dark-text dark:text-light-color">
-              Log in to your account
+            <h1 className="text-3xl font-semibold max-2xl:text-2xl mb-0 text-dark-text dark:text-light-color">
+              Continue with:
             </h1>
             <a
               href={`${backendUrl}/auth/google`}
