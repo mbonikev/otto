7c1be1c855600516dd1d93fa25b5bc92447fe272
mbonikev <mbonimpayekevin@gmail.com>
2025-02-07 14:41:02 +0200

    updated src/components/LoginModal/LoginModal.jsx

--- src/components/LoginModal/LoginModal.jsx
+++ src/components/LoginModal/LoginModal.jsx
@@ -8,7 +8,7 @@ import { PiChatsCircleFill } from "react-icons/pi";
 function LoginModal() {
 
   return (
-    <div className="w-[300px] h-[300px] p-2">
+    <div className="w-[320px] h-[300px] p-2">
       <div className="w-full h-full bg-white dark:bg-card-dark-1 rounded-2xl p-2 flex flex-col">
 
       </div>
