15edf8f54b505ed3bc30829af9b5eb555fb53c09
mbonikev <mbonimpayekevin@gmail.com>
2025-02-07 23:22:46 +0200

    updated src/pages/Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -24,7 +24,7 @@ const Login = () => {
             </h1>
             <a
               href={`${backendUrl}/auth/google`}
-              className="bg-white dark:bg-card-dark-1 hover:bg-stone-100 dark:hover:bg-card-hover-dark ring-1 ring-stone-200 dark:ring-card-dark-1 text-dark-text dark:text-light-color py-2.5 px-3 w-full max-md:min-w-fit max-md:pr-5 max-lg:text-sm flex items-center justify-center gap-2 rounded-full"
+              className="bg-white dark:bg-card-dark-1 hover:bg-stone-100 dark:hover:bg-card-hover-dark ring-1 ring-stone-200 shadow-md dark:ring-card-dark-1 text-dark-text dark:text-light-color py-2.5 px-3 w-full max-md:min-w-fit max-md:pr-5 max-lg:text-sm flex items-center justify-center gap-2 rounded-full"
             >
               <FcGoogle className="text-3xl" />
               <p className="min-w-[65px] font-medium">Google</p>
