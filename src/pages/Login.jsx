f99b81d16fd03629634968a2aa35a03911b7168f
mbonikev <mbonimpayekevin@gmail.com>
2025-02-07 20:49:39 +0200

    updated src/pages/Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -39,7 +39,7 @@ const Login = () => {
             </div>
           </div>
         </div>
-        <div className="w-3/5 h-full max-xl:hidden flex flex-col items-end justify-center gap-6 bg-stone-100 dark:bg-card-dark-1/40 overflow-hidden p-20 relative">
+        <div className="w-3/5 h-full max-xl:hidden flex flex-col items-end justify-center gap-6 bg-stone-100 dark:bg-card-dark-1/40 overflow-hidden p-2 relative">
           <h1 className="absolute top-5 right-7 text-dark-text-weak dark:text-light-color-weak text-sm flex items-center gap-2 font-medium">
             <FaPlay className="text-xs opacity-40" />
             Demo
