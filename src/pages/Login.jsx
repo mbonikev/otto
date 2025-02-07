6bc56b04a09dfcdca5a8301878854208c5978d94
mbonikev <mbonimpayekevin@gmail.com>
2025-02-07 21:43:33 +0200

    updated src/pages/Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -40,7 +40,7 @@ const Login = () => {
           </div>
         </div>
         <div className="w-[72%] h-full max-xl:hidden flex flex-col items-end justify-center gap-6 overflow-hidden p-2 relative">
-         
+          <div className="bg-stone-100 dark:bg-card-dark-1/40 w-full h-full"></div>
         </div>
       </div>
     </div>
