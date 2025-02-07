7333d252aba765f028b17fac80c99f58080dbea4
mbonikev <mbonimpayekevin@gmail.com>
2025-02-07 15:46:43 +0200

    updated src/pages/Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -13,7 +13,7 @@ const Login = () => {
         <div className="h-full flex-1 flex flex-col items-start justify-start gap-6 w-full p-6">
           <Link to={"/"} className="w-fit h-fit flex gap-2">
             <img src="./logo.png" className="h-9" />
-            <h1 className="font-Kanit text-2xl font-light text-dark-text-weak text-center">
+            <h1 className="font-Kanit text-2xl font-light text-dark-text-weak dark:text-light-color-weak text-center">
               Otto
             </h1>
           </Link>
