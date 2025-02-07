e6d9bf89649be179c8cc851664626571757ea505
mbonikev <mbonimpayekevin@gmail.com>
2025-02-07 14:30:51 +0200

    updated src/pages/Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,7 +13,7 @@ import { RiLoader2Fill } from "react-icons/ri";
 import Cookies from "js-cookie";
 import axios from "axios";
 
-function Home({handleOpenLoginModal}) {
+function Home({ handleOpenLoginModal, handleCloseModal }) {
   const { user, models } = useOutletContext() || {};
   const { displayName, photo, email, userId } = user || {};
   const [messages, setMessages] = useState([]);
