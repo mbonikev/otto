47aea70afed2615245c1d29b84839c943969decd
mbonikev <mbonimpayekevin@gmail.com>
2025-02-07 15:03:06 +0200

    updated src/components/LoginModal/LoginModal.jsx

--- src/components/LoginModal/LoginModal.jsx
+++ src/components/LoginModal/LoginModal.jsx
@@ -4,8 +4,10 @@ import { Link, useLocation } from "react-router-dom";
 import Cookies from "js-cookie";
 import { RiLoader2Fill } from "react-icons/ri";
 import { PiChatsCircleFill } from "react-icons/pi";
+import { FcGoogle } from "react-icons/fc";
 
 function LoginModal() {
+  const backendUrl = import.meta.env.VITE_BACKEND_API;
   return (
     <div className="w-[320px] h-fit p-2">
       <div className="w-full h-full bg-white dark:bg-card-dark-1 text-dark-text dark:text-light-color rounded-2xl p-5 flex flex-col gap-3">
@@ -15,6 +17,13 @@ function LoginModal() {
         <p className="text-xl text-dark-text dark:text-light-color">
           Log in to enable full feature functionality.
         </p>
+        <a
+          href={`${backendUrl}/auth/google`}
+          className="bg-stone-200/60 hover:bg-stone-200 ring-1 ring-stone-200 text-dark-text py-2.5 px-3 w-fit min-w-[260px] max-md:min-w-fit max-md:pr-5 max-lg:text-sm flex items-center justify-start gap-3 rounded-full"
+        >
+          <FcGoogle className="text-3xl" />
+          Continue with Google
+        </a>
       </div>
     </div>
   );
