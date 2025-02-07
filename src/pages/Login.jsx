f8b5c8516a81f777ce443ccaf9a77dc378479c44
mbonikev <mbonimpayekevin@gmail.com>
2025-02-07 20:58:00 +0200

    updated src/pages/Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -45,7 +45,7 @@ const Login = () => {
             Demo
           </h1>
           <div className="w-full h-full overflow-hidden rounded-[30px] shadow-xl shadow-stone-200 dark:shadow-black/60 relative px-20">
-            <div className="w-fit min-h-full rounded-3xl bg-white p-0">
+            <div className="w-fit h-fit rounded-3xl bg-white p-0">
               <ReactPlayer
                 url="./assets/tutorial.mp4"
                 controls={false}
