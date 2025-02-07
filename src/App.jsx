6e1b57087413c0c53d73b64a83f004a05d46c297
mbonikev <mbonimpayekevin@gmail.com>
2025-02-07 14:05:27 +0200

    updated src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,35 +13,59 @@ function App() {
     return <NotFound />;
   }
   return (
-    <HashRouter>
-      <Routes>
-        {/* Protected Routes for Logged-In Users */}
-        <Route element={<ProtectedRoutes />}>
-          <Route path="/" element={<Home />} />
-        </Route>
-
-        {/* Prevent Logged-In Users from Accessing Login & Chat */}
-        <Route
-          path="/login"
-          element={
-            <AuthRedirect>
-              <Login />
-            </AuthRedirect>
-          }
+    <>
+    {/* Overlay */}
+    {loginModal && (
+        <div
+          onClick={handleCloseModal}
+          className={`fixed top-0 left-0 w-full h-svh bg-black/15 dark:bg-black/30 z-50 transition-opacity duration-300 ${
+            animateLoginModal ? "opacity-100" : "opacity-0"
+          }`}
         />
-        <Route
-          path="/chat"
-          element={
-            <AuthRedirect>
-              <Chat />
-            </AuthRedirect>
-          }
-        />
+      )}
 
-        {/* Catch-All Route */}
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </HashRouter>
+      {/* login Modal */}
+      {loginModal && (
+        <div
+          className={`w-fit h-svh fixed top-0 left-0 transition-transform duration-300 ease-in-out z-30
+                ${
+                  animateLoginModal ? "translate-x-0" : "-translate-x-[300px]"
+                }`}
+        >
+          <ChatHistory convs={convs} loadingConvs={loadingConvs} />
+        </div>
+      )}
+      
+      <HashRouter>
+        <Routes>
+          {/* Protected Routes for Logged-In Users */}
+          <Route element={<ProtectedRoutes />}>
+            <Route path="/" element={<Home />} />
+          </Route>
+
+          {/* Prevent Logged-In Users from Accessing Login & Chat */}
+          <Route
+            path="/login"
+            element={
+              <AuthRedirect>
+                <Login />
+              </AuthRedirect>
+            }
+          />
+          <Route
+            path="/chat"
+            element={
+              <AuthRedirect>
+                <Chat />
+              </AuthRedirect>
+            }
+          />
+
+          {/* Catch-All Route */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </HashRouter>
+    </>
   );
 }
 
