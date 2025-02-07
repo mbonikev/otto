c7401a7948987b48859515658805af0a4501407d
mbonikev <mbonimpayekevin@gmail.com>
2025-02-07 14:53:44 +0200

    updated src/components/LoginModal/LoginModal.jsx

--- src/components/LoginModal/LoginModal.jsx
+++ src/components/LoginModal/LoginModal.jsx
@@ -6,11 +6,14 @@ import { RiLoader2Fill } from "react-icons/ri";
 import { PiChatsCircleFill } from "react-icons/pi";
 
 function LoginModal() {
-
   return (
     <div className="w-[320px] h-[300px] p-2">
       <div className="w-full h-full bg-white dark:bg-card-dark-1 rounded-2xl p-2 flex flex-col">
-
+        <h1>Log in</h1>
+        <p>
+          Please log in to access your account and enable full feature
+          functionality.
+        </p>
       </div>
     </div>
   );
