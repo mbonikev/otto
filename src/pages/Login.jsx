35829755f6d25bcb27147fc5534036ca1b0bf420
mbonikev <mbonimpayekevin@gmail.com>
2025-02-07 21:48:24 +0200

    updated src/pages/Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -25,7 +25,7 @@ const Login = () => {
               href={`${backendUrl}/auth/google`}
               className="bg-white dark:bg-card-dark-1 hover:bg-stone-200 dark:hover:bg-card-hover-dark ring-1 ring-stone-200 dark:ring-card-dark-1 text-dark-text dark:text-light-color py-2 px-3 w-full max-md:min-w-fit max-md:pr-5 max-lg:text-sm flex items-center justify-center gap-3 rounded-full"
             >
-              <FcGoogle className="text-3xl" />
+              <FcGoogle className="text-2xl" />
               Continue with Google
             </a>
           </div>
