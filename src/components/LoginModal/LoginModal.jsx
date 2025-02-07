18ec0e974418b7e3e34b9c8117e7bb70a0bf1292
mbonikev <mbonimpayekevin@gmail.com>
2025-02-07 14:42:02 +0200

    updated src/components/LoginModal/LoginModal.jsx

--- src/components/LoginModal/LoginModal.jsx
+++ src/components/LoginModal/LoginModal.jsx
@@ -9,7 +9,7 @@ function LoginModal() {
 
   return (
     <div className="w-[320px] h-[300px] p-2">
-      <div className="w-full h-full bg-white dark:bg-[#252525] rounded-2xl p-2 flex flex-col">
+      <div className="w-full h-full bg-white dark:bg-[#282828] rounded-2xl p-2 flex flex-col">
 
       </div>
     </div>
