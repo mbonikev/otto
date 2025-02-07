d961af2b67d561c61ccf8da139f8e41a641d9c24
mbonikev <mbonimpayekevin@gmail.com>
2025-02-08 01:13:13 +0200

    updated src/pages/Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -56,6 +56,9 @@ const Login = () => {
           <div className=" w-full h-full rounded-tl-3xl overflow-hidden dark:bg-body-dark pt-4 pl-10 hidden dark:block">
           <img src="./assets/screenshot_dark.png" className="w-fit h-full object-cover object-left-top " />
           </div>
+          <div className=" w-full h-full rounded-tl-3xl overflow-hidden dark:bg-body-dark pt-4 pl-10 hidden dark:block">
+          <img src="screenshot_dark.png" className="w-fit h-full object-cover object-left-top " />
+          </div>
         </div>
       </div>
     </div>
