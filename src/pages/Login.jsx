0a205672ec0e91e0c73c03631b14ba8b6eceec27
mbonikev <mbonimpayekevin@gmail.com>
2025-02-07 20:59:30 +0200

    updated src/pages/Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -54,6 +54,7 @@ const Login = () => {
                 playing={true}
                 width="100%"
                 height="100%"
+                quality={10}
               />
             </div>
           </div>
