b0856a7bf91e0f28dac8ff43524ddcf6bb543f7a
mbonikev <mbonimpayekevin@gmail.com>
2025-02-07 21:00:40 +0200

    updated src/pages/Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -45,14 +45,14 @@ const Login = () => {
             Demo
           </h1>
           <div className="w-full h-full overflow-hidden rounded-[30px] shadow-xl shadow-stone-200 dark:shadow-black/60 relative">
-            <div className="w-full h-full rounded-3xl bg-white p-0">
+            <div className="w-full h-full rounded-3xl bg-white p-20">
               <ReactPlayer
                 url="/assets/tutorial.mp4"
                 controls={false}
                 muted={true}
                 loop={true}
                 playing={true}
-                width="100%"
+                width="auto"
                 height="fit-content"
                 config={{
                   file: {
