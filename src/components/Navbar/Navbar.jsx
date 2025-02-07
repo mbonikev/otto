8a5776e5e220fd341a804a1aebfb41cfc364239a
mbonikev <mbonimpayekevin@gmail.com>
2025-02-07 13:56:56 +0200

    updated src/components/Navbar/Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -147,7 +147,7 @@ function Navbar({
   return (
     <div className="w-full h-fit bg-white/80 dark:bg-body-dark/80 backdrop-blur-lg grid grid-cols-3 text-dark-text dark:text-light-color px-3 py-2 sticky top-0">
       {/* Overlay */}
-      {chatsModal && (
+      {loginModal || chatsModal && (
         <div
           onClick={handleCloseModal}
           className={`fixed top-0 left-0 w-full h-full bg-black/15 dark:bg-black/30 z-20 transition-opacity duration-300 ${
