e1d0854863ae0afe2cd242c66201532067288ec0
mbonikev <mbonimpayekevin@gmail.com>
2025-02-07 13:39:14 +0200

    updated src/pages/NotFound.jsx

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function NotFound() {
   return (
-    <div className='p-4 text-sm text-dark-text dark:text-light-color font-mono'>Page Not Found go back <a href="/" className='text-blue-500 underline '>/Home</a></div>
+    <div className='p-4 text-sm text-dark-text dark:text-light-color font-mono'>Page Not Found, Go back <a href="/" className='text-blue-500 underline '>/Home</a></div>
   )
 }
 
