9b75bd93ef85c2d07c54e12861265b6a59456d12
mbonikev <mbonimpayekevin@gmail.com>
2025-02-07 23:48:49 +0200

    updated src/pages/Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,6 @@
 import { FaGoogle, FaPlay } from "react-icons/fa";
-import { FaApple, FaFacebook } from "react-icons/fa6";
+import { FaApple } from "react-icons/fa6";
 import { FcGoogle } from "react-icons/fc";
-import { IoLogoGithub } from "react-icons/io";
 import ReactPlayer from "react-player";
 import { Link } from "react-router-dom";
 
@@ -10,28 +9,28 @@ const Login = () => {
   document.title = "Login - Otto";
   return (
     <div className="h-svh flex justify-start items-center flex-col">
-      <div className="w-full h-full flex p-3 gap-3">
-        <div className="h-full flex-1 flex flex-col items-start justify-start gap-6 w-full p-4">
+      <div className="w-full h-full flex p-3">
+        <div className="h-full flex-1 flex flex-col items-start justify-start gap-6 w-full p-0">
           <Link to={"/"} className="w-fit h-fit flex gap-2">
-            <img src="./logo.png" className="h-7" />
-            <h1 className="font-Kanit text-xl font-normal text-dark-text dark:text-light-color text-center">
+            <img src="./logo.png" className="h-8" />
+            <h1 className="font-Kanit text-xl font-medium text-dark-text dark:text-light-color text-center">
               Otto
             </h1>
           </Link>
           <div className="w-full flex-1 flex flex-col items-start justify-start gap-3 pt-[5vh]">
-            <h1 className="text-2xl font-semibold mb-1 text-dark-text dark:text-light-color">
-              Continue with
+            <h1 className="text-3xl font-semibold max-2xl:text-2xl mb-4 text-dark-text dark:text-light-color">
+              Log in to your account
             </h1>
             <a
               href={`${backendUrl}/auth/google`}
-              className="bg-white dark:bg-card-dark-1 hover:bg-stone-100 dark:hover:bg-card-hover-dark ring-1 ring-stone-200 dark:ring-card-dark-1 text-dark-text dark:text-light-color py-2.5 px-3 w-full max-md:min-w-fit max-md:pr-5 max-lg:text-sm flex items-center justify-center gap-2 rounded-full"
+              className="bg-white dark:bg-card-dark-1 hover:bg-stone-200 dark:hover:bg-card-hover-dark ring-1 ring-stone-200 dark:ring-card-dark-1 text-dark-text dark:text-light-color py-2 px-3 w-full max-md:min-w-fit max-md:pr-5 max-lg:text-sm flex items-center justify-center gap-3 rounded-full"
             >
               <FcGoogle className="text-3xl" />
-              <p className="min-w-[65px] font-medium">Google</p>
+              Continue with Google
             </a>
           </div>
           <div className="w-full flex items-center justify-between p-1 flex-wrap gap-2 max-lg:justify-center">
-            <h1 className="text-dark-text-weak dark:text-light-color-weak text-sm font-normal">
+            <h1 className="text-dark-text-weak dark:text-light-color-weak text-sm font-medium">
               &copy; Otto 2025. All rights reserved.
             </h1>
             <div className="flex items-center justify-end gap-3 text-dark-text-weak dark:text-light-color-weak text-sm">
