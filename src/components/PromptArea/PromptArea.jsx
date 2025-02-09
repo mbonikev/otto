1a75d4ef6c9cf450095718bbc731ce6bc04a1cd1
mbonikev <mbonimpayekevin@gmail.com>
2025-02-09 13:00:57 +0200

    updated src/components/PromptArea/PromptArea.jsx

--- src/components/PromptArea/PromptArea.jsx
+++ src/components/PromptArea/PromptArea.jsx
@@ -239,7 +239,7 @@ function PromptArea({
             {showModels && (
               <div
                 ref={modelsRef}
-                className=" w-[300px] max-sm:w-full h-[300px] bg-white dark:text-light-color dark:bg-card-dark-2 ring-1 ring-stone-300/80 dark:ring-dark-text-weak/50 shadow-md rounded-2xl absolute left-0 bottom-[130%] py-2 pl-2 flex flex-col"
+                className=" w-[300px] max-sm:w-full h-[300px] bg-white dark:text-light-color dark:bg-card-dark-2 ring-1 ring-stone-300/80 dark:ring-dark-text-weak/50 shadow-md rounded-2xl absolute left-0 bottom-[130%] flex flex-col"
               >
                 <div className="h-[40px] w-full mb-2 bg-red-200">
                   <div className="w-full h-full bg-transparent rounded-xl flex items-center justify-start">
