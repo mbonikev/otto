a00fd6b4b36ce8d9529b0079e7599bb144bab854
mbonikev <mbonimpayekevin@gmail.com>
2025-02-09 13:38:22 +0200

    updated src/components/Navbar/Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -257,7 +257,7 @@ function Navbar({
             className="absolute top-[120%] w-[220px] h-fit rounded-2xl ring-1 bg-white dark:bg-card-dark-1 ring-stone-200 dark:ring-card-dark-1 shadow-lg p-1.5"
           >
             {/* <button className="w-full rounded-xl p-2 flex items-center justify-start gap-2 hover:bg-stone-100 text-dark-text-weak hover:text-dark-text dark:text-light-color dark:hover:text-light-color dark:hover:bg-light-color-weak/10">
-              <HiOutlineUser className="text-2xl text-dark-text-weak/70 dark:text-light-color-weak" />
+              <HiOutlineUser className="text-xl text-dark-text-weak/70 dark:text-light-color-weak" />
               <h1 className="text-sm">My account</h1>
             </button> */}
             <button className="w-full rounded-xl p-2 flex items-center justify-start gap-2 hover:bg-stone-100 text-dark-text-weak hover:text-dark-text dark:text-light-color dark:hover:text-light-color dark:hover:bg-light-color-weak/10">
