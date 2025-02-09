a8695f63ae030b7be56f5dc7a74e498fad2fe63f
mbonikev <mbonimpayekevin@gmail.com>
2025-02-09 13:02:58 +0200

    updated src/components/PromptArea/PromptArea.jsx

--- src/components/PromptArea/PromptArea.jsx
+++ src/components/PromptArea/PromptArea.jsx
@@ -239,9 +239,9 @@ function PromptArea({
             {showModels && (
               <div
                 ref={modelsRef}
-                className=" w-[300px] max-sm:w-full h-[300px] bg-white dark:text-light-color dark:bg-card-dark-2 ring-1 ring-stone-300/80 dark:ring-dark-text-weak/50 shadow-md rounded-2xl absolute left-0 bottom-[130%] flex flex-col"
+                className=" w-[300px] max-sm:w-full h-[300px] bg-white dark:text-light-color dark:bg-card-dark-2 ring-1 ring-stone-300/80 dark:ring-dark-text-weak/50 shadow-md rounded-2xl absolute left-0 bottom-[130%]  flex flex-col"
               >
-                <div className="h-[40px] w-full mb-2 bg-red-200">
+                <div className="h-[40px] w-full mb-2 border-b-[1px] dark:border-light-color-weak/40">
                   <div className="w-full h-full bg-transparent rounded-xl flex items-center justify-start">
                     <HiMiniMagnifyingGlass className="mx-3 text-lg min-w-fit" />
                     <input
