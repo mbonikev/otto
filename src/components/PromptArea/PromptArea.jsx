2d843dac2775b1fc21defd75b5cdfeb00e19b179
mbonikev <mbonimpayekevin@gmail.com>
2025-02-09 13:29:30 +0200

    updated src/components/PromptArea/PromptArea.jsx

--- src/components/PromptArea/PromptArea.jsx
+++ src/components/PromptArea/PromptArea.jsx
@@ -281,7 +281,7 @@ function PromptArea({
                             {owner}
                           </h1>
                           {/* Models List */}
-                          <div className="w-full h-fit flex flex-col mb-[5px]">
+                          <div className="w-full h-fit flex flex-col mb-[8px]">
                             {ownerModels
                               .filter((model) => model.active)
                               .map((model) => (
