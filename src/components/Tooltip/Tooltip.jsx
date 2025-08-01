bb0ae2956e45d97a98d6ee4093443c3085607dd6
mbonikev <mbonimpayekevin@gmail.com>
2025-08-01 11:01:13 +0200

    updated src/components/Tooltip/Tooltip.jsx

--- src/components/Tooltip/Tooltip.jsx
+++ src/components/Tooltip/Tooltip.jsx
@@ -4,7 +4,7 @@ import { Fade } from "react-awesome-reveal";
 function Tooltip({ title, placement, shortcut }) {
   return (
     <div
-      className={`absolute z-10 w-fit h-fit whitespace-nowrap px-3 py-[7px] rounded-[9px] bg-[#222222] dark:bg-sidebar-color text-white/90 text-sm font-semibold pointer-events-none opacity-0 group-hover:opacity-100 delay-150 flex flex-col items-center ${
+      className={`absolute z-10 w-fit h-fit whitespace-nowrap px-3 py-[7px] rounded-[9px] bg-[#393939] dark:bg-sidebar-color text-white/90 text-sm font-semibold pointer-events-none opacity-0 group-hover:opacity-100 delay-150 flex flex-col items-center ${
         placement === "left"
           ? "top-[120%] left-0"
           : placement === "center"
@@ -17,7 +17,7 @@ function Tooltip({ title, placement, shortcut }) {
       }`}
     >
       <div
-        className={`w-2 h-2 bg-[#222222] dark:bg-sidebar-color absolute rotate-45 ${
+        className={`w-2 h-2 bg-[#393939] dark:bg-sidebar-color absolute rotate-45 ${
           placement === "left"
             ? "-top-[4px] left-[16px]"
             : placement === "center"
